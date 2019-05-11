727e1d4a5de5dd1c7740552d9752ec5c5ee3af31
Charles Ai <kailun.ai27@gmail.com>
2019-05-11 04:50:07 -0400

    Home 页框架完成

---
+++ src/components/FooterComp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+function Footer(props) {
+    return(
+        <div>
+           <Container>
+
+           </Container> 
+        </div>
+    );
+}
+
+export default Footer;
