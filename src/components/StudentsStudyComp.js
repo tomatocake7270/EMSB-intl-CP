c089e05ddcfd0114b9af53f6f99833b00b4af988
Charles-AI <kailun.ai27@gmail.com>
2019-05-13 10:59:55 -0400

    add RenderCard and cardInfo for Study

--- src/components/StudentsStudyComp.js
+++ src/components/StudentsStudyComp.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Container, Row, Col, Media, Card, CardImg, CardBody } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { titleInfo } from '../shared/StudentsDetails';
+import { titleInfo, cardInfo } from '../shared/StudentsDetails';
 
-import Students from './StudentsComp';
+// import Students from './StudentsComp';
 
 const RenderMedia = ({href, url, title}) => {
     return (
@@ -25,11 +25,37 @@ const RenderMedia = ({href, url, title}) => {
     )
 }
 
+const RenderCard = ({ imgUrl, iconUrl, altText, heading }) => {
+    return (
+        <Col md="6" className="my-1">
+            <Card className="border rounded">
+                <CardImg top width="100%" src={imgUrl} alt={altText} className="img-fluid"/>
+                <CardBody className="bg-success">
+                    <Media>
+                        <Media left href="#">
+                            <Media object src={iconUrl} alt={altText} className="img-fluid students-field-trip-icon"/>
+                        </Media>
+                        <Media body className="ml-2 mt-1">
+                            <Media heading>
+                                {heading}
+                            </Media>
+                            <div className="d-flex justify-content-between">
+                                <p className="students-field-trip-location"></p>
+                                <FontAwesomeIcon icon={ faInfoCircle } size="lg"/>
+                            </div>
+                        </Media>
+                    </Media>
+                </CardBody>
+            </Card>
+        </Col>
+    )
+}
+
 function StudentsStudy() {    
     return(
         <div>
-            <Students laMian = "I love Ramian so much!" />
-            {/* <Container className="my-2 border-bottom border-primary pb-1">
+            {/* <Students laMian = "I love Ramian so much!" /> */}
+            <Container className="my-2 border-bottom border-primary pb-1">
                 <div className="d-flex justify-content-center">
                     <RenderMedia title = {titleInfo.fieldTrips.title}
                                  href  = {titleInfo.fieldTrips.href}
@@ -45,27 +71,11 @@ function StudentsStudy() {
             </Container>
             <Container>
                 <Row className="my-2">
-                    <Col md="6" className="my-1">
-                        <Card className="border rounded">
-                            <CardImg top width="100%" src="/assets/images/fieldTrips/ziplining.jpg" alt="Ziplining trip" className="img-fluid"/>
-                            <CardBody className="bg-success">
-                                <Media>
-                                    <Media left href="#">
-                                        <Media object src="/assets/images/fieldTrips/ziplining-bg.png" alt="Ziplining icon" className="img-fluid students-field-trip-icon"/>
-                                    </Media>
-                                    <Media body className="ml-2 mt-1">
-                                        <Media heading>
-                                            Learn English/French on Rosetta Stone
-                                        </Media>
-                                        <div className="d-flex justify-content-between">
-                                            <p className="students-field-trip-location"></p>
-                                            <FontAwesomeIcon icon={ faInfoCircle } size="lg"/>
-                                        </div>
-                                    </Media>
-                                </Media>
-                            </CardBody>
-                        </Card>
-                    </Col>
+                    <RenderCard imgUrl  = {cardInfo.rosettaStone.imgUrl}
+                                iconUrl = {cardInfo.rosettaStone.iconUrl}
+                                altText = {cardInfo.rosettaStone.altText}
+                                heading = {cardInfo.rosettaStone.heading} />
+                    
                     <Col md="6" className="my-1">
                         <Card>
                             <CardImg top width="100%" src="/assets/images/fieldTrips/ski.jpg" alt="Ski trip" className="img-fluid"/>
@@ -74,50 +84,16 @@ function StudentsStudy() {
                             </CardBody>
                         </Card>
                     </Col>
-                    <Col md="6" className="my-1">
-                        <Card >
-                            <CardImg top width="100%" src="/assets/images/fieldTrips/qc-city.jpg" alt="Quebec City trip" className="img-fluid"/>
-                            <CardBody className="bg-secondary">
-                                <Media>
-                                    <Media left href="#" className="mt-1">
-                                        <Media object src="/assets/images/fieldTrips/qc-city-bg.png" alt="Quebec City icon" className="img-fluid students-field-trip-icon"/>
-                                    </Media>
-                                    <Media body className="ml-2 mt-2">
-                                        <Media heading>
-                                            Online Tutoring on LEARN Quebec
-                                        </Media>
-                                        <div className="d-flex justify-content-between">
-                                            <p className="students-field-trip-location"></p>
-                                            <FontAwesomeIcon icon={ faInfoCircle } size="lg"/>
-                                        </div>
-                                    </Media>
-                                </Media>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                    <Col md="6" className="my-1">
-                        <Card>
-                            <CardImg top width="100%" src="/assets/images/fieldTrips/sugar-shack.jpg" alt="Sugar Shack trip" className="img-fluid"/>
-                            <CardBody className="bg-warning">
-                                <Media>
-                                    <Media left href="#">
-                                        <Media object src="/assets/images/fieldTrips/sugar-shack-bg.png" alt="Sugar Shack icon" className="img-fluid students-field-trip-icon"/>
-                                    </Media>
-                                    <Media body className="ml-2 mt-1">
-                                        <Media heading>
-                                            Learn and Test English on Vital English
-                                        </Media>
-                                        <div className="d-flex justify-content-between">
-                                            <p className="students-field-trip-location"></p>
-                                            <FontAwesomeIcon icon={ faInfoCircle } size="lg"/>
-                                        </div>
-                                    </Media>
-                                </Media>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container> */}
+                    <RenderCard imgUrl  = {cardInfo.learnQuebec.imgUrl}
+                                iconUrl = {cardInfo.learnQuebec.iconUrl}
+                                altText = {cardInfo.learnQuebec.altText}
+                                heading = {cardInfo.learnQuebec.heading} />
+                    <RenderCard imgUrl  = {cardInfo.vitalEnglish.imgUrl}
+                                iconUrl = {cardInfo.vitalEnglish.iconUrl}
+                                altText = {cardInfo.vitalEnglish.altText}
+                                heading = {cardInfo.vitalEnglish.heading} />
+                </Row>  
+            </Container>
         </div>
     )
 }
