70e8dfb6f972ed7335df60ee5bd96b463746547e
Charles-AI <kailun.ai27@gmail.com>
2019-05-17 15:40:44 -0400

    Update school detail

--- src/shared/SchoolDetails.js
+++ src/shared/SchoolDetails.js
@@ -4,23 +4,23 @@ export const SCHOOLS =
             id: 0,
             name: 'FACE School',
             route: 'face',
-            image: [
-                '',
-                '',
-                ''
+            imgUrl: [
+                '/assets/images/schools/FACE-1.jpg',
+                '/assets/images/schools/FACE-1.jpg',
+                '/assets/images/schools/FACE-1.jpg'
             ],
-            intro: '',
-            address: '',
-            telephone: '',
-            email: '',
-            website: '',
+            intro: 'Fine Arts Core Education (FACE) High School is located north of Sherbrooke St. West and is accessible by bus, McGill Metro, and Central or Windsor Train Stations.',
+            address: '3449 University Street, Montreal, QC, H3A 2A8',
+            telephone: '(514) 350-8899',
+            email: 'face@emsb.qc.ca',
+            website: 'http://face-english.csdm.ca',
             philo: ''
         },
         {
             id: 1,
             name: 'James Lyng High School',
-            route: 'jlh',
-            image: [
+            route: 'jameslyng',
+            imgUrl: [
                 '',
                 '',
                 ''
@@ -35,7 +35,8 @@ export const SCHOOLS =
         {
             id: 2,
             name: 'John F. Kennedy High School',
-            image: [
+            route: 'johnfkennedy',
+            imgUrl: [
                 '',
                 '',
                 ''
