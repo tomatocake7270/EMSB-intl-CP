da5cb828d3d5a70a1a845d852862bab21068f04b
Charles Ai <kailun.ai27@gmail.com>
2019-05-24 04:58:26 -0400

    Prepare for dynamic table generation

--- src/components/AdminAnnounceComp.js
+++ src/components/AdminAnnounceComp.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { Table, Badge, Container, Row, Col, Form, Input, Button } from 'reactstrap';
+import React, { useState } from 'react';
+import { Table, Badge, Form, Input, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faPlus, faPlusCircle, faMinusCircle, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faPlus, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const AdminAnnounce = () => {
+
+    const [ oprState, setOprState ] = useState({
+        showOpr: false,
+    });
+
+    const displayOpr = () => {
+        setOprState({ showOpr: true });
+    }
+
+    const hideOpr = () => {
+        setOprState({ showOpr: false });
+    }
+
+    //let RowOperation = null;
+    let rowOprBtnClass = "d-none";
+    
+    if (oprState.showOpr) {
+        rowOprBtnClass = "admin-table-opr-icon";
+    }
+    
     return (
         <div className="admin-content d-flex justify-content-start">
             {/* Left section */}
@@ -31,7 +51,7 @@ const AdminAnnounce = () => {
                             </Button>
                         </Form>
                         <Button color="danger" className="admin-new-btn">
-                                <FontAwesomeIcon icon={faPlus} size="lg" className="admin-new-icon" />
+                            <FontAwesomeIcon icon={faPlus} size="lg" className="admin-new-icon" />
                         </Button>
                     </div>
                 </div>
@@ -57,7 +77,7 @@ const AdminAnnounce = () => {
 
                 {/* Table row */}
                 <Table hover borderless className="admin-right-table text-center">
-                    <thead>
+                    <thead className="border-bottom">
                     <tr>
                         <th className="lato-font bold-font">Sender</th>
                         <th className="lato-font bold-font">Create time</th>
@@ -68,52 +88,75 @@ const AdminAnnounce = () => {
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">EMSB</th>
-                        <td>
-                            <p className="admin-table-date">16:39</p>
-                            <p className="admin-table-date">2019-05-29</p>
-                        </td>
-                        <td>
-                            <p className="admin-table-date">16:39</p>
-                            <p className="admin-table-date">2019-05-29</p>
-                        </td>
+                    <tr onMouseOver={displayOpr} onMouseOut={hideOpr}>
+                        <th scope="row">
+                            EMSB
+                            <p className="d-none">1</p>
+                        </th>
+                        <td><p className="admin-table-time">16:39 <br /> 2019-05-29</p></td>
+                        <td><p className="admin-table-time">17:46 <br /> 2019-06-01</p></td>
                         <td><Badge color="danger">Draft</Badge></td>
                         <td className="text-left">The new EMSB Admin App developed especially for admin users is just available for download on the App Store!</td>
                         <td className="d-flex justify-content-center">
-                            <FontAwesomeIcon icon={faPlusCircle} className="admin-table-opr-icon" />
+                            <Button color="link" className="admin-table-opr-btn">
+                                <FontAwesomeIcon icon={faEdit} className={rowOprBtnClass} />
+                            </Button>
+                            <Button color="link" className="admin-table-opr-btn">
+                                <FontAwesomeIcon icon={faTrashAlt} className={rowOprBtnClass} />
+                            </Button>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">EMSB Intl</th>
-                        <td>16:39, 2019-05-29</td>
-                        <td>17:20, 2019-05-29</td>
+                        <th scope="row">
+                            EMSB Intl
+                            <p className="d-none">2</p>
+                        </th>
+                        <td><p className="admin-table-time">16:39 <br /> 2019-05-29</p></td>
+                        <td><p className="admin-table-time">17:46 <br /> 2019-06-01</p></td>
                         <td><Badge color="success">Published</Badge></td>
                         <td className="text-left">The new EMSB Admin App developed especially for admin users is just available for download on the App Store!</td>
                         <td className="d-flex justify-content-center">
-                            <FontAwesomeIcon icon={faEdit} className="admin-table-opr-icon" />
-                            <FontAwesomeIcon icon={faTrashAlt} className="admin-table-opr-icon" />
-                            <FontAwesomeIcon icon={faMinusCircle} className="admin-table-opr-icon" />
+                            <Button color="link" className="admin-table-opr-btn">
+                                <FontAwesomeIcon icon={faEdit} className="admin-table-opr-icon"  />
+                            </Button>
+                            <Button color="link" className="admin-table-opr-btn">
+                                <FontAwesomeIcon icon={faTrashAlt} className="admin-table-opr-icon-trash"  />
+                            </Button>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">EMSB Intl</th>
-                        <td>16:39, 2019-05-29</td>
-                        <td>17:20, 2019-05-29</td>
+                        <th scope="row">
+                            EMSB Intl
+                            <p className="d-none">3</p>
+                        </th>
+                        <td><p className="admin-table-time">16:39 <br /> 2019-05-29</p></td>
+                        <td><p className="admin-table-time">17:46 <br /> 2019-06-01</p></td>
                         <td><Badge color="success">Published</Badge></td>
                         <td className="text-left">The new EMSB Admin App developed especially for admin users is just available for download on the App Store!</td>
                         <td className="d-flex justify-content-center">
-                            <FontAwesomeIcon icon={faPlusCircle} className="admin-table-opr-icon" />
+                            <Button color="link" className="admin-table-opr-btn d-none">
+                                <FontAwesomeIcon icon={faEdit} className="admin-table-opr-icon"  />
+                            </Button>
+                            <Button color="link" className="admin-table-opr-btn d-none">
+                                <FontAwesomeIcon icon={faTrashAlt} className="admin-table-opr-icon-trash"  />
+                            </Button>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">EMSB</th>
-                        <td>16:39, 2019-05-29</td>
-                        <td>17:20, 2019-05-29</td>
+                        <th scope="row">
+                            EMSB <p className="d-none">4</p>
+                        </th>
+                        <td><p className="admin-table-time">16:39 <br /> 2019-05-29</p></td>
+                        <td><p className="admin-table-time">17:46 <br /> 2019-06-01</p></td>
                         <td><Badge color="danger">Draft</Badge></td>
                         <td className="text-left">The new EMSB Admin App developed especially for admin users is just available for download on the App Store!</td>
                         <td className="d-flex justify-content-center">
-                            <FontAwesomeIcon icon={faPlusCircle} className="admin-table-opr-icon" />
+                            <Button color="link" className="admin-table-opr-btn d-none">
+                                <FontAwesomeIcon icon={faEdit} className="admin-table-opr-icon"  />
+                            </Button>
+                            <Button color="link" className="admin-table-opr-btn d-none">
+                                <FontAwesomeIcon icon={faTrashAlt} className="admin-table-opr-icon-trash"  />
+                            </Button>
                         </td>
                     </tr>
                     </tbody>
